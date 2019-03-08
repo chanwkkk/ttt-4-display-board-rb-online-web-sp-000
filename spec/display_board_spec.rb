fa05f51f76b039b5258750c8e80f2421b1425d69
Yingqi Chen <chanwk18@hotmail.com>
2019-03-08 07:37:52 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -178,11 +178,11 @@ describe "#display_board in 'lib/display_board.rb" do
       output = capture_puts{ display_board(board) }
       rows = output.split("\n")
 
-      expect(rows[0]).to eq(" O | O | O ")
+      expect(rows[0]).to eq(" X | X | X ")
       expect(rows[1]).to eq("-----------")
-      expect(rows[2]).to eq(" O | O | O ")
+      expect(rows[2]).to eq(" X | X | X ")
       expect(rows[3]).to eq("-----------")
-      expect(rows[4]).to eq(" O | O | O ")
+      expect(rows[4]).to eq(" X | X | X ")
     end
 
     it 'prints an entire board full of Os' do
