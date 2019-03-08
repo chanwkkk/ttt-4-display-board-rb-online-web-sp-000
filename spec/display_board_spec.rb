e7336ae4a24a706fb2eef49f760556ef188eabc3
Yingqi Chen <chanwk18@hotmail.com>
2019-03-08 07:33:16 +0000

    Automatically backed up by Learn

--- spec/display_board_spec.rb
+++ spec/display_board_spec.rb
@@ -172,7 +172,7 @@ describe "#display_board in 'lib/display_board.rb" do
        expect(rows[4]).to eq("   |   |   ")
 
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
+      # expect(true).to be(true)
     end
 
     it 'prints an entire board full of Os' do
@@ -184,7 +184,17 @@ describe "#display_board in 'lib/display_board.rb" do
       # example's situation.
 
       # *** Comment the line below by adding a # at the line start ***
-      expect(true).to be(true)
+    #  expect(true).to be(true)
+    board = ["O", "O", "O", "O", "O", "O", "O", "O", "O"]
+
+    output = capture_puts{ display_board(board) }
+    rows = output.split("\n")
+
+    expect(rows[0]).to eq(" X | O | X ")
+    expect(rows[1]).to eq("-----------")
+    expect(rows[2]).to eq(" O | X | X ")
+    expect(rows[3]).to eq("-----------")
+    expect(rows[4]).to eq(" O | X | O ")
     end
   end
 end
